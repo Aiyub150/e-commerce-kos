aa8246c0dff12cfaaed6fc720166b890995b2a7e
aiyub heriyanto <aiyubheriyanto150@gmail.com>
2024-11-20 20:08:26 +0700

    add basic authcontroller

--- routes/web.php
+++ routes/web.php
@@ -4,23 +4,7 @@ use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-<<<<<<< HEAD
     return view('welcome');
-=======
-    return view('index');
-});
-
-// Route::get('/login', function () {
-//     return view('login');
-// });
-
-// Route::get('/register', function () {
-//     return view('register');
-// });
-
-Route::get('/dashboard', function () {
-    return view('admin/dashboard');
->>>>>>> b3c282a (add basic authcontroller)
 });
 
 
